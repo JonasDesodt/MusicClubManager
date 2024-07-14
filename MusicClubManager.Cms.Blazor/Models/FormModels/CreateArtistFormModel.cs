ba5649f6c4ff3d6c67d472ec39365ee0141e5d0a
JonasDesodt <95071893+JonasDesodt@users.noreply.github.com>
2024-07-14 09:50:27 +0200

    create edit artist

---
+++ MusicClubManager.Cms.Blazor/Models/FormModels/CreateArtistFormModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicClubManager.Cms.Blazor.Models.FormModels
+{
+    public class CreateArtistFormModel
+    {
+        [Required(ErrorMessage = "Name is a required property.")]
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Image { get; set; }
+    }
+}
