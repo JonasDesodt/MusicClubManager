c8898978cf2f445908abab10af3aa0e09a85367a
JonasDesodt <95071893+JonasDesodt@users.noreply.github.com>
2024-07-27 15:28:47 +0200

    create artist (w/ image selection)

---
+++ MusicClubManager.Cms.Blazor/Models/Contexts/CreateArtistContext.cs
@@ -0,0 +1,9 @@
+﻿using MusicClubManager.Cms.Blazor.Models.FormModels;
+
+namespace MusicClubManager.Cms.Blazor.Models.Contexts
+{
+    public class CreateArtistContext
+    {
+        public required CreateArtistFormModel Model { get; set; }
+    }
+}
