137bee54f641bc081438408d06f1dcd6687b1994
JonasDesodt <95071893+JonasDesodt@users.noreply.github.com>
2024-07-30 11:11:29 +0200

    ToMultipartFormDataContent

---
+++ MusicClubManager.Cms.Blazor/Extensions/CreateImageFormModelExtensions.cs
@@ -0,0 +1,25 @@
+﻿using MusicClubManager.Cms.Blazor.Models.FormModels;
+
+namespace MusicClubManager.Cms.Blazor.Extensions
+{
+    public static class CreateImageFormModelExtensions
+    {
+        public static MultipartFormDataContent? ToMultipartFormDataContent(this CreateImageFormModel model)
+        {
+            if (model.BrowserFile is not { Size: > 0 } file)
+            {
+                return null;
+            }
+
+            var content = new MultipartFormDataContent();
+            var fileContent = new StreamContent(file.OpenReadStream());
+            fileContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(file.ContentType);
+            content.Add(fileContent, "file", file.Name);
+
+            content.Add(new StringContent(model.Alt ?? file.Name), "Alt");
+            content.Add(new StringContent(file.ContentType), "ContentType");
+
+            return content;
+        }
+    }
+}
---
+++ MusicClubManager.Cms.Blazor/Extensions/SelectImageExtensions.cs
@@ -0,0 +1,12 @@
+﻿using MusicClubManager.Cms.Blazor.Pages.Artist;
+
+namespace MusicClubManager.Cms.Blazor.Extensions
+{
+    public static class SelectImageExtensions
+    {
+        public static void Test(this SelectImage component)
+        {
+
+        }
+    }
+}
