140e8c0de3aa4ee78a321547e98cb13faeec889e
JonasDesodt <95071893+JonasDesodt@users.noreply.github.com>
2024-07-31 09:54:18 +0200

    base class for image selection

---
+++ MusicClubManager.Cms.Blazor/Extensions/PaginationExtensions.cs
@@ -0,0 +1,17 @@
+﻿using MusicClubManager.Cms.Blazor.Models.Contexts;
+using MusicClubManager.Dto.Transfer;
+
+namespace MusicClubManager.Cms.Blazor.Extensions
+{
+    public static class PaginationExtensions
+    {
+        public static PaginationRequest ToPaginationRequest(this Pagination pagination)
+        {
+            return new PaginationRequest
+            {
+                Page = (uint)pagination.Page,
+                PageSize = (uint)pagination.PageSize,
+            };
+        }
+    }
+}
